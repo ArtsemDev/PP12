bb6814425894b3e7757aa8a78736e08af1d2ffd7
ArtsemDev <thommy.python.dev@gmail.com>
2023-08-23 21:21:11 +0300

    ///

--- main/blog/urls.py
+++ main/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, register_converter
 
-from .views import CategoryListView, PostListView, PostSearchListView
+from .views import PostListView
 from .converters import ULIDConverter
 
 
@@ -8,8 +8,5 @@ register_converter(converter=ULIDConverter, type_name='uuuu')
 
 
 urlpatterns = [
-    path('form/', PostSearchListView.as_view()),
-    path('<slug:slug>/', PostListView.as_view(), name='blog_post_list'),
-    path('', CategoryListView.as_view(), name='blog_index'),
-    # path('<ulid:post_ulid>/', )
+    path('', PostListView.as_view(), name='blog_index'),
 ]
--- main/blog/views.py
+++ main/blog/views.py
@@ -6,39 +6,45 @@ from .models import Category, Post
 from .forms import SearchPostForm, CategoryForm
 
 
-class CategoryListView(ListView):
-    template_name = 'blog/index.html'
-    model = Category
+# class CategoryListView(ListView):
+#     template_name = 'blog/index.html'
+#     model = Category
+#
+#
+# class PostListView(ListView):
+#     template_name = 'blog/post-list.html'
+#     model = Post
+#     context_object_name = 'post_list'
+#
+#     def get_queryset(self):
+#         category_slug = self.kwargs.get('slug')
+#         category = get_object_or_404(Category, slug=category_slug)
+#         return get_list_or_404(Post, category=category)
+#
+#
+# class PostSearchListView(ListView):
+#     template_name = 'blog/form.html'
+#     model = Post
+#
+#     def get_queryset(self):
+#         queryset = self.model.objects.filter(is_published=True)
+#         if self.request.GET.get('category'):
+#             category = Category.objects.filter(name__contains=self.request.GET.get('category').title())
+#
+#             if category:
+#                 category = category[0]
+#                 queryset = queryset.filter(category=category)
+#         return queryset
+#
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data()
+#         form = SearchPostForm()
+#         context['form'] = form
+#         context['category_form'] = CategoryForm()
+#         return context
 
 
 class PostListView(ListView):
-    template_name = 'blog/post-list.html'
     model = Post
-    context_object_name = 'post_list'
-
-    def get_queryset(self):
-        category_slug = self.kwargs.get('slug')
-        category = get_object_or_404(Category, slug=category_slug)
-        return get_list_or_404(Post, category=category)
-
-
-class PostSearchListView(ListView):
-    template_name = 'blog/form.html'
-    model = Post
-
-    def get_queryset(self):
-        queryset = self.model.objects.filter(is_published=True)
-        if self.request.GET.get('category'):
-            category = Category.objects.filter(name__contains=self.request.GET.get('category').title())
-
-            if category:
-                category = category[0]
-                queryset = queryset.filter(category=category)
-        return queryset
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data()
-        form = SearchPostForm()
-        context['form'] = form
-        context['category_form'] = CategoryForm()
-        return context
+    template_name = 'blog/index.html'
+    ordering = '-date_created'
